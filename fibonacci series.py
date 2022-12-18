12325e2ef54c169932ac54deb79a9baaef39aba7
sketchdinee317 <120928122+sketchdinee317@users.noreply.github.com>
2022-12-18 17:37:04 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 18 14:01:51 2022
+
+@author: admin
+"""
+
+first=0
+second=1
+for i in range(0,10):
+    print(first)
+    third=first
+    first=second
+    second=third+second
